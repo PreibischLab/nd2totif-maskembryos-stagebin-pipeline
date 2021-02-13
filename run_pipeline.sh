cf1985db439ffb3110b58746695d8e3f076cf80e
bellonet <bellonet@gmail.com>
2021-02-13 12:45:11 +0100

    add git pull embryos.csv before run

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,9 @@
 export OMP_NUM_THREADS=1
 
+cd /data/preibisch/Laura_Microscopy/dosage_compensation/smFISH-analysis/fit/embryos_csv/
+git pull origin master
+cd /scratch/AG_Preibisch/Ella/embryo/nd2totif_maskembryos_stagebin_pipeline
+
 python 0_copy_to_scratch.py 2>&1 |  tee -a pipeline.log
 qsub qsub_run.sh
 ##### Need to run script 6_xxxx manually
