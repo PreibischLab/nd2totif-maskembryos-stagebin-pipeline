f83276bfe46e82c0ef15770bd47a62a8a9e7626a
bellonet <bellonet@gmail.com>
2021-05-04 18:27:41 +0200

    test

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,6 +6,6 @@ cd /scratch/AG_Preibisch/Ella/embryo/nd2totif_maskembryos_stagebin_pipeline
 
 python 0_copy_to_scratch.py 2>&1 |  tee -a pipeline.log
 ####### commented to check that git pull works first
-qsub qsub_run.sh
+#qsub qsub_run.sh
 ##### Need to run script 6_xxxx manually
 
